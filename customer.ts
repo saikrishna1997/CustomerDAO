18323d1d3269d0705f763e8383cea4d729b99b17
saikrishna1997 <31088933+saikrishna1997@users.noreply.github.com>
2019-12-18 18:06:09 +0530

    Create customer.ts

---
+++ customer.ts
@@ -0,0 +1,32 @@
+export class customer{
+
+    public cust_id:number;
+	public name:string;
+	public dob:string;
+	public contact:number;
+	public address:string;
+	public username:string;
+	public password:string;
+	public aadhar_card:number;
+	public pan_card:string;
+	public branch_id:string;
+	
+
+constructor(cust_id:number, name:string, dob:string, contact:number, address:string, username:string,password:string, aadhar_card:number, pan_card:string, branch_id:string)
+{
+	this.cust_id=cust_id;
+	this.name=name;
+	this.dob=dob;
+	this.contact=contact;
+	this.address=address;
+	this.username=username;
+	this.password=password;
+	this.aadhar_card=aadhar_card;
+	this.branch_id=branch_id;
+	this.pan_card=pan_card;
+	this. branch_id= branch_id;
+	
+	
+}
+
+}
